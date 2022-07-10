cafdb761bb4a09b7f62c9d685043037583129575
mhdibaqri <mahdi2000bm@gmail.com>
2022-07-10 23:27:10 +0430

    fix bugs

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,3 +1,13 @@
 <?php 
     include 'const.php';
+    include 'config.php';
+
+    $dsn = "mysql:dbname=$dbConfig->dbname;host=$dbConfig->hostname;";
+    try {
+        $conn = new PDO($dsn, "root", "");
+    } catch (PDOException $result_e) {
+        die($result_e);
+    }
+
     include 'libs/helper.php';
+    include 'libs/libs-location.php';
--- process/record-location.php
+++ process/record-location.php
@@ -3,5 +3,18 @@
       die('invalid request!');    
     }
 
-    var_dump($_POST);
-    
+    include 'bootstrap/init.php';
+
+    if(isset($_POST['action']))
+    {
+      switch ($_POST['action']){
+        case 'record-business':
+          insertLocation($_POST);
+        break;
+        default:
+            echo 'invalid request!';
+        break;
+      }
+    }
+
+
